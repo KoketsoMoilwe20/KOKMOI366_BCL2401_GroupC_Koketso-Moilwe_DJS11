dc8112174f46269722c8a94546c11ca1e29382b0
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-06-21 10:29:27 +0200

    create Series detail component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import PageNotFound from "./pages/PageNotFound"
+import Series from "./pages/Series/Series"
+import SeriesDetail from "./pages/Series/SeriesDetail"
 
 
 export default function App() {
@@ -13,6 +15,8 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
+          <Route path="/series" element={<Series />} />
+          <Route path="/series/:id" element={<SeriesDetail />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/Series/Series.jsx
+++ src/pages/Series/Series.jsx
@@ -49,5 +49,5 @@ export default function Series() {
         )
       }
         </div>
-      )
+      );
 }
